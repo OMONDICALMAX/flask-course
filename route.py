7e52fefb0a1a7177e087106c65882317edc46052
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-09-02 05:21:51 +0300

    Repairing the poject's structure

---
+++ route.py
@@ -0,0 +1,17 @@
+
+
+@app.route('/')
+@app.route('/home')
+
+def home_page():
+  return render_template('index.html')
+
+
+@app.route('/market')
+def market_page():
+
+  items=Item.query.all()
+  return render_template ('market.html', items=items)
+
+if __name__ == '__main__':
+  app.run(debug=True)
