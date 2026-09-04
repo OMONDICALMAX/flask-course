13374feb57b0d0291ef9ef58c1756d8e172632a2
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-09-04 06:05:31 +0300

    morning practice

--- market/__init__.py
+++ market/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+app.config['SECRET_KEY'] = '4e2ad04a96a00881dd9ded54'
 db = SQLAlchemy(app)
 
 from market import route
--- market/route.py
+++ market/route.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template
 from market.module import Item
+from market.forms import RegisterForm
 
 @app.route('/')
 @app.route('/home')
@@ -14,6 +15,12 @@ def market_page():
 
   items=Item.query.all()
   return render_template ('market.html', items=items)
+
+@app.route('/register')
+def register_page():
+  form = RegisterForm()
+  return render_template('register.html', form=form)
+
 
 if __name__ == '__main__':
   app.run(debug=True)
