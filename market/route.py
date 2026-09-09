c2dd8a1f8e4d52c9ad38afe2ded4c10cb264101f
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-09-09 20:11:38 +0300

    interrupted

--- market/route.py
+++ market/route.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.module import Item
+from flask import render_template, redirect, url_for
+from market.module import Item, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route('/')
 @app.route('/home')
@@ -19,6 +20,13 @@ def market_page():
 @app.route('/register')
 def register_page():
   form = RegisterForm()
+  if form.validate_on_submit():
+    user_to_create = User( username=form.username,
+                          email_address=form.email_address.data,
+                          password1=form.password1.data)
+    db.session.add(user_to_create)
+    db.session.commit()
+    return redirect(url_for('market_page'))
   return render_template('register.html', form=form)
 
 @app.route('/login')
