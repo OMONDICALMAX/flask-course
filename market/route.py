12a6091e05e545cce89c9f44ed92f0306277134e
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-09-10 06:11:06 +0300

    morning lessons

--- market/route.py
+++ market/route.py
@@ -1,5 +1,5 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.module import Item, User
 from market.forms import RegisterForm
 from market import db
@@ -17,16 +17,19 @@ def market_page():
   items=Item.query.all()
   return render_template ('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
   form = RegisterForm()
   if form.validate_on_submit():
-    user_to_create = User( username=form.username,
+    user_to_create = User( username=form.username.data,
                           email_address=form.email_address.data,
-                          password1=form.password1.data)
+                          password_hash=form.password1.data)
     db.session.add(user_to_create)
     db.session.commit()
     return redirect(url_for('market_page'))
+  if form.errors != {}: # If there are no errors arising from the validations in forms.py
+    for err_msg in form.errors.values():
+      flash(f'There was an error with creating a user: {err_msg}')
   return render_template('register.html', form=form)
 
 @app.route('/login')
